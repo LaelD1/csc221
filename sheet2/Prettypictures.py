224761cff6550a5706068b43a3cdd90f08379f9e
LaelD1 <lbd19388@email.vccs.edu>
2023-07-10 06:15:34 -0700

    Add mathquiz.py

---
+++ sheet2/Prettypictures.py
@@ -0,0 +1,15 @@
+from gasp import *
+
+begin_graphics()
+
+Circle((300, 200), 200)
+Circle((200, 250), 25)
+Circle((400, 250), 25)
+Line((270, 170), (300, 230))
+Line((270, 170), (325, 170))
+Arc((300, 200), 150, 225, 315)
+
+
+update_when('key_pressed') 
+end_graphics()
+de
